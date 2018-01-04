ee5dbb01ab30f57eb1d6ebeff4c2a57abc9e995c
wanglanwei <dreamcatchwang1991@gmail.com>
2018-01-04 23:49:08 +0800

    udpate code

--- binarysorttree/binarysorttree.c
+++ binarysorttree/binarysorttree.c
@@ -3,6 +3,7 @@
 #include <assert.h>
 
 // 二叉排序树
+// TODO: @gatsby 优化代码
 
 typedef int ElementType;
 typedef int KeyType;
@@ -57,6 +58,126 @@ Node *find_node(BSTree tree, KeyType key) {
 }
 
 
-void insert_node(Node *tree, KeyType key, ElementType data) {
+void insert_node(BSTree *tree, KeyType key, ElementType data) {
+    // 搜索二叉排序树
+    Node *cur = *tree;
+    Node *parent = NULL;
+    while(cur != NULL) {
+        parent = cur;
+        if(key == cur->key) {
+            // 查找到节点，则修改节点
+            cur->data = data;
+            return ;
+        } else if(key < cur->key) {
+            // 查找左子树
+            cur = cur->lchild;
+        } else {
+            // 查找右子树
+            cur = cur->rchild;
+        }
+    }
+    // 新建节点
+    Node *new_node = (Node *)malloc(sizeof(Node));
+    if(!new_node) {
+        return ;
+    }
+    new_node->key = key;
+    new_node->data = data;
+    new_node->lchild = new_node->rchild = NULL;
+    // 空树则将新节点直接当做根节点
+    if(!parent) {
+        *tree = new_node;
+    } else if(key < parent->key) {
+        parent->lchild = new_node;
+    } else {
+        parent->rchild = new_node;
+    }
     return ;
+}
+
+
+void delete_node(BSTree *tree, KeyType key) {
+    // 从二叉排序树tree上删除key标识的节点
+    // 如果查找到目标节点，且目标节点为叶子节点，则直接删除节点
+    // 如果目标节点仅有左子树或右子树则直接删除，删除节点后需要找到直接前驱与直接后继节点
+    if(!*tree) {
+        return ;
+    }
+    Node *cur = *tree;
+    Node *parent = NULL;
+    Node *prev, *prev_parent = NULL; // 目标节点的直接前驱节点，直接前驱节点的父节点
+    while(cur != NULL) {
+        if(key == cur->key) {
+            // 查找到节点
+            break;
+        } else if(key < cur->key) {
+            // 查找左子树
+            parent = cur;
+            cur = cur->lchild;
+        } else {
+            // 查找右子树
+            parent = cur;
+            cur = cur->rchild;
+        }
+    }
+#ifndef NDEBUG
+    printf("delete_node find node: %p\n", cur);
+#endif
+    // 查找到节点
+    if(!cur) {
+        if(!cur->lchild) {
+            // 如果目标节点不存在左子树
+        } else if(!cur->rchild) {
+            // 如果目标节点不存在右子树
+        } else {
+            // 左转
+            prev = cur->lchild;
+            while(prev->rchild) {
+                prev_parent = prev;
+                prev = prev->rchild;
+            }
+            // 此时prev指向待删除节点的直接前驱节点
+            prev->lchild = cur->lchild;
+            prev->rchild = cur->rchild;
+            if(prev->key < parent->key) {
+                parent->lchild = prev;
+            } else {
+                parent->rchild = prev;
+            }
+            if(!prev_parent) {
+                prev_parent->rchild = NULL;
+            }
+            free((void *)cur);
+            cur = NULL;
+        }
+    }
+    return ;
+}
+
+
+int main(void) {
+    int a[] = {1, 3, 5, 6, 8, 10, 12, 13};
+    int n = sizeof(a) / sizeof(int);
+    BSTree tree = NULL;
+    // 构建二叉排序树
+    int i;
+    for(i=0; i<n; i++) {
+        insert_node(&tree, a[i], a[i]);
+    }
+    // 搜索二叉排序树
+    Node *p = find_node(tree, 10);
+#ifndef NDEBUG
+    printf("pointer of p: %p\n", p);
+#endif
+    if(p) {
+        printf("find node vlaue: %d\n", p->data);
+    }
+    delete_node(&tree, 10);
+    p = find_node(tree, 10);
+#ifndef NDEBUG
+    printf("pointer of p: %p\n", p);
+#endif
+    if(p) {
+        printf("find node vlaue: %d\n", p->data);
+    }
 }
