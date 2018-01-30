b4f3c7f29cdd2accc5024f067c4bf74f32aa1ef8
wanglanwei <dreamcatchwang1991@gmail.com>
2018-01-30 14:24:08 +0800

    add lis.

--- algorithms/lis.c
+++ algorithms/lis.c
@@ -3,10 +3,28 @@
 #include <stdlib.h>
 
 
-void lis() {
-    // 5, 3, 4, 8, 6, 7
-    // d(1) = 1
-    // d(2) = 1
-    // d(3) = 1 + d(2)
-    // d(4) = 1 + max{d(1), d(2), d(3)}+1
+int lis(int *arr, int n) {
+    int *d = (int *)malloc(sizeof(int) * n);
+    if(!d) {
+        return -1;
+    }
+    free((void *)d);
+    int len = 1;
+    int i, j;
+    for(i=0; i<n; i++) {
+        d[i] = 1;
+        for(j=0; j<i; j++) {
+            if(arr[j] <= arr[i] && (d[j]+1 > d[i])) {
+                d[i] = d[j]+1;
+            }
+        }
+        len = d[i] > len ? d[i] : len;
+    }
+    return len;
+}
+
+int main() {
+    int arr[] = {2, 7, 1, 5, 6, 4, 3, 8, 9};
+    printf("lis: %d\n", lis(arr, sizeof(arr)/sizeof(int)));
+    return 0;
 }
